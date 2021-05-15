caf6f1857463602e8f5425df8e39acb0e8b6b6a0
jolbol1 <github@jamesshopland.com>
2021-05-15 21:34:10 +0100

    Add output function for a clean terminal output

--- script.js
+++ script.js
@@ -2,22 +2,30 @@
 
 //Get Random Number between 0 and number given in parameter (-1)
 const getRandNum = number => Math.floor(Math.random() * number);
-
-
+//Capatalise the first letter of the given string. This allows us to use the key and look nice.
+const capatalise = word => word.charAt(0).toUpperCase() + word.slice(1);
 // Set up an object where each key is a charachter and each value and array of  5 of their quotes
 const quotes = {
     thor: ['I went for the head', 'We’re the same, you and I. Just a couple of hot-headed fools', 'Do I look to be in a gaming mood?', 'So, Earth has wizards now, huh?', 'Bring me Thanos!'],
     spiderman: ['Your friendly neighborhood Spider-Man!', 'Who am I? I\'m Spider-Man!', "I will never forget these words: 'With great power, comes great responsibility.'", 'What we believe we know may not be the truth.', 'You have a metal arm? Dude, that is so awesome!'],
     ironman: ['I am Iron Man.', 'Genius, billionaire, playboy, philanthropist.', 'We have a Hulk.', 'Sometimes you gotta run before you can walk.', 'Doth mother know you weareth her drapes.']
 }
-
 //Create an empty array to store a quote from each superhero.
 let selectedQuotes = [];
-
+// Run through all the heros and grab a random quote from each.
 for(let hero in quotes) {
     let quoteIndex = getRandNum(quotes[hero].length);
-    selectedQuotes.push(`"${quotes[hero][quoteIndex]}" - ${hero}`);
+    selectedQuotes.push(`"${quotes[hero][quoteIndex]}" - ${capatalise(hero)}`);
+}
+//Fucntion to allow a easy clean output ot the terminal.
+function outputMessage(selectedQuotes) {
+    console.log(`Welcome Stranger!`)
+    console.log('The Avengers have some advice for you');
+    console.log('---------------------------------------')
+    console.log(selectedQuotes.join('\n'))
+    console.log('---------------------------------------')
+    console.log('End of Avengers Wisdom')
 }
 
-console.log(selectedQuotes);
+outputMessage(selectedQuotes);
 
