05b4fbdf2ff0894cb42aeaab2a08605163935b16
jolbol1 <github@jamesshopland.com>
2021-05-15 20:52:55 +0100

    Added the first quotes in an object

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // The start of the file. Ready for VC
+
+// Set up an object where each key is a charachter and each value and array of  5 of their quotes
+const quotes = {
+    thor: ['I went for the head', 'We’re the same, you and I. Just a couple of hot-headed fools', 'Do I look to be in a gaming mood?', 'So, Earth has wizards now, huh?', 'Bring me Thanos!'],
+    spiderman: ['Your friendly neighborhood Spider-Man!', 'Who am I? I\'m Spider-Man!', "I will never forget these words: 'With great power, comes great responsibility.'", 'What we believe we know may not be the truth.', 'You have a metal arm? Dude, that is so awesome!'],
+    ironman: ['I am Iron Man.', 'Genius, billionaire, playboy, philanthropist.', 'We have a Hulk.', 'Sometimes you gotta run before you can walk.', 'Doth mother know you weareth her drapes.']
+}
