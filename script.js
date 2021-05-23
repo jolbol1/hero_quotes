e2d5c86df07c6bec55bf56e16e3a33d36a62dad5
jolbol1 <github@jamesshopland.com>
2021-05-23 18:57:32 +0100

    make JavaScript expandable. Change CSS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Set the refresh button to a variable.
 let refreshButton = document.getElementById('new-wisdom');
+let wisdomBlock = document.getElementById('wisdom-block');
+
 //Get Random Number between 0 and number given in parameter (-1)
 const getRandNum = number => Math.floor(Math.random() * number);
 //Capatalise the first letter of the given string. This allows us to use the key and look nice.
@@ -8,34 +10,59 @@ const capatalise = word => word.charAt(0).toUpperCase() + word.slice(1);
 const quotes = {
     thor: ['I went for the head', 'We’re the same, you and I. Just a couple of hot-headed fools', 'Do I look to be in a gaming mood?', 'So, Earth has wizards now, huh?', 'Bring me Thanos!'],
     spiderman: ['Your friendly neighborhood Spider-Man!', 'Who am I? I\'m Spider-Man!', "I will never forget these words: 'With great power, comes great responsibility.'", 'What we believe we know may not be the truth.', 'You have a metal arm? Dude, that is so awesome!'],
-    ironman: ['I am Iron Man.', 'Genius, billionaire, playboy, philanthropist.', 'We have a Hulk.', 'Sometimes you gotta run before you can walk.', 'Doth mother know you weareth her drapes.']
-}
-let paddingAmount = 14;
-// Run through all the heros and grab a random quote from each. Returns an array with a random quote from each hero.
-function getRandomQuotes() {
-    let selectedQuotes = [];
-    for(let hero in quotes) {
-        let quoteIndex = getRandNum(quotes[hero].length);
-        const sentenceStart = `${capatalise(hero)} said`
-        const quote = quotes[hero][quoteIndex];
-        const paddingNeeded = paddingAmount - sentenceStart.length
-        const padding = (paddingNeeded !== 0) ? (' ').repeat(paddingNeeded) : '';
-        selectedQuotes.push(`${sentenceStart}${padding}| "${quote}"`);
+    ironman: ['I am Iron Man.', 'Genius, billionaire, playboy, philanthropist.', 'We have a Hulk.', 'Sometimes you gotta run before you can walk.', 'Doth mother know you weareth her drapes.'],
+    getRandomHeroQuote(hero) {
+        let herosQuotes = this[hero];
+        let quoteIndex = getRandNum(herosQuotes.length);
+        return herosQuotes[quoteIndex]
+    },
+    getRandomQuotes() {
+        let randomHeroQuotes = {};
+        for(let hero in this) {
+            if(typeof this[hero] === 'function') {
+                continue;
+            }
+            console.log(hero);
+            randomHeroQuotes[hero] = this.getRandomHeroQuote(hero);
+        }
+        return randomHeroQuotes;
     }
-    return selectedQuotes;
+
 }
 
-//Display the quotes in the HTML. This grabs the random quote array from above.
+//This function generates the Div's required to display the quotes in HTML format. I did it this way as it can be easily expanded by just adding more heros to the object.
 function displayQuotes() {
-    let selectedQuotes = getRandomQuotes();
-    document.getElementById('wisdom-one').innerHTML = "<p class='quotes' id='hero1'>" + selectedQuotes[0] + "</p>";
-    document.getElementById('wisdom-two').innerHTML = "<p class='quotes' id='hero2'>" + selectedQuotes[1] + "</p>";
-    document.getElementById('wisdom-three').innerHTML = "<p class='quotes' id='hero3'>" + selectedQuotes[2] + "</p>";
+    wisdomBlock.innerHTML = '';
+    let selectedQuotes = quotes.getRandomQuotes();
+    for(let hero in selectedQuotes) {
+        // Create the block for the quote and hero name
+        const heroDiv = document.createElement("div");
+        heroDiv.setAttribute("class", "hero-block");
+        heroDiv.setAttribute("id", hero);
+        // Create the block for the hero name
+        const heroNameDiv = document.createElement("div");
+        const nameText = document.createTextNode(hero);
+        heroNameDiv.appendChild(nameText);
+        heroNameDiv.setAttribute("class", "hero-name");
+        // Create the block for the hero quote
+        const heroQuoteDiv = document.createElement("div");
+        const quoteText = document.createTextNode(selectedQuotes[hero]);
+        heroQuoteDiv.appendChild(quoteText);
+        heroQuoteDiv.setAttribute("class", "hero-quote");
+
+        // Return the hero div for each hero
+        heroDiv.appendChild(heroNameDiv);
+        heroDiv.appendChild(heroQuoteDiv);
+        wisdomBlock.appendChild(heroDiv);
+    }
 }
 // Add the event listener for on click of the quotes.
 refreshButton.onclick = displayQuotes;
 // Make sure there is wi
 displayQuotes();
+console.log(quotes.getRandomHeroQuote('thor'))
+console.log(quotes.getRandomQuotes());
+console.log(quotes.length)
 
 
 
